e0cce3900f17b3dcb2bff702f2550ea724748265
cocodrips <cocodrips@gmail.com>
2018-12-20 11:09:31 +0900

    Simple api document.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+def main(arg1, arg2):
+    """
+    Summary of packages/classes/functions in one line. 
+
+    descriptions.
+
+    Args:
+        arg1(int): description.
+        arg2(str): description.
+
+    Returns:
+        int: description.
+    """
+    return 0
+
